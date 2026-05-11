97a956fd9181ff608d6e1a1eced705773393cc9a
Riski Setiawan <anandariskisetiawan3@gmail.com>
2026-05-11 21:17:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Hapus base: './' atau base: '/' biar pakai default
-  build: {
-    outDir: 'dist',
-    assetsDir: '.', // Ini kuncinya! Biar file JS/CSS gak masuk folder assets, tapi sejajar sama index.html
-    emptyOutDir: true
-  },
   server: {
     port: 8080,
     host: true
