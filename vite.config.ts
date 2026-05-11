8246c339c979f1d2f4b4ddde5b14b74d070be968
Riski Setiawan <anandariskisetiawan3@gmail.com>
2026-05-11 21:31:59 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '', // Kosongkan total, jangan pakai '/' atau './'
+  build: {
+    outDir: 'dist',
+    assetsDir: '', // Kosongkan biar gak ada folder /assets/
+    emptyOutDir: true
+  },
   server: {
     port: 8080,
     host: true
