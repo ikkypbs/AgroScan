66e9ff72a6dad94c48ae80b78513f2cd82e7550a
Riski Setiawan <anandariskisetiawan3@gmail.com>
2026-05-11 16:18:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Tambahkan ini biar jalurnya relatif dan nggak putih polos
+  base: '', // Pakai string kosong supaya path-nya fleksibel di Cloud Run
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 8080,
     host: true,
