395341d70f4e13824ce0dbac9d54416b0944e0c3
Riski Setiawan <anandariskisetiawan3@gmail.com>
2026-05-11 16:25:43 +0700

    Update base path and assets directory in Vite config
    Change base path for deployment and update assets directory.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // Pakai string kosong supaya path-nya fleksibel di Cloud Run
+  base: '/', 
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
   },
   server: {
     port: 8080,
